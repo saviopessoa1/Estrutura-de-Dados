739fedebb8b1c74689763c1bf9e7b6591b03c0b5
Sávio Neri Pessoa <saviopessoa345@gmail.com>
2025-11-10 20:24:30 -0300

    Commit

---
+++ Estruturas de Dados Unidade-2/fila-dinamica/src/main/java/br/ifba/vca/bsi/BuubleSort.java
@@ -0,0 +1,16 @@
+package br.ifba.vca.bsi;
+
+import java.lang.reflect.Array;
+
+public class BuubleSort {
+for (int i = 0; i < Array.getLength(args); i++) {
+    if (array[i] > array[i + 1]) {
+        swap (i, i + 1);
+    }
+    public void swap (int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+}
+}
+    }
